9b5d6cf83cfe7acc9b30dccf7dadbd5f91abca3f
prodigy-genes <bulletoze@gmail.com>
2025-07-02 19:30:56 +0000

    Add Anonymous provider to authentication configuration

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,11 +1,6 @@
-import { convexAuth, getAuthUserId } from "@convex-dev/auth/server";
-import { Password } from "@convex-dev/auth/providers/Password";
-import { Anonymous } from "@convex-dev/auth/providers/Anonymous";
+// convex/auth.ts
+import { getAuthUserId } from "@convex-dev/auth/server";
 import { query } from "./_generated/server";
-
-export const { auth, signIn, signOut, store, isAuthenticated } = convexAuth({
-  providers: [Password, Anonymous],
-});
 
 export const loggedInUser = query({
   handler: async (ctx) => {
